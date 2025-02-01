3665db22ee1666116e109f0abcc4bb10aa18b599
Dima <zimoykin@gmail.com>
2025-02-01 17:24:59 +0100

    add github-authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { Skill } from './skills/entities/skill.entity';
 import { JwtModule } from '@zimoykin/auth';
 import { CategoriesModule } from './categories/categories.module';
 import { Category } from './categories/entity/category.entity';
+import { AuthModule } from './auth/auth.module';
+import { GithubModule } from './github/github.module';
+import { User } from './auth/entity/user.entity';
 
 @Module({
   imports: [
@@ -30,19 +33,21 @@ import { Category } from './categories/entity/category.entity';
       inject: [ConfigService],
       useFactory: (config: ConfigService<ConfigVariables>) => {
         return {
-          type: 'mysql',
+          type: 'mariadb',
           host: config.get<string>('DB_CONNECTION')!,
           port: config.get<number>('DB_PORT')!,
           username: config.get<string>('DB_USERNAME')!,
           password: config.get<string>('DB_PASSWORD')!,
           database: config.get<string>('DB_DATABASE')!,
           synchronize: process.env.NODE_ENV !== 'prod',
-          entities: [Skill, Category],
+          entities: [Skill, Category, User],
         };
       },
     }),
     SkillsModule,
     CategoriesModule,
+    AuthModule,
+    GithubModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/service-config.ts
+++ src/service-config.ts
@@ -9,6 +9,11 @@ const schema = {
   DB_DATABASE: Joi.string().required(),
   DB_PORT: Joi.number().default(3306),
   JWT_SECRET: Joi.string().required(),
+  GITHUB_CLIENT_ID: Joi.string().required(),
+  GITHUB_CLIENT_SECRET: Joi.string().required(),
+  GITHUB_CALLBACK_URL: Joi.string().required(),
+  SESSION_SECRET: Joi.string().required(),
+  FRONTEND_GITHUB_AUTH_PAGE: Joi.string().required(),
   MONGO_CONNECTION: Joi.string()
     .uri({ scheme: ['mongodb', 'mongodb+srv'] })
     .optional(),
